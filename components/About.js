94eede5deff1e9fd10e67d937e8581e82892a6f5
ESHA AMIR <75741369+eshaamir@users.noreply.github.com>
2024-07-06 23:11:28 +0500

    Add files via upload

---
+++ components/About.js
@@ -0,0 +1,13 @@
+// About.js
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Learn about our company and team.</p>
+    </div>
+  );
+}
+
+export default About;
