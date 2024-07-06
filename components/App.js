2d88b3f68ce607f40668b4fbc5abd52e57a8c2f1
ESHA AMIR <75741369+eshaamir@users.noreply.github.com>
2024-07-06 23:10:11 +0500

    App.js file was initially added

---
+++ components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import './App.css';
+
+
+function App() {
+  return (
+    <Router>
+    <div>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  </Router>
+  );
+}
+
+export default App;
